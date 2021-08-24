4d8418fb923741b60ad42f6b0ae1ccf929be2e4d
Shafaullah Malik <72027876+MuhammadShafaullah@users.noreply.github.com>
2021-08-24 17:13:56 +0500

    Update Nodemcu_post.ino

--- Nodemcu_post.ino
+++ Nodemcu_post.ino
@@ -3,10 +3,11 @@
 SoftwareSerial mySerialn(D1, D2); //Rx,TX
 //==================================
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-
+#include <WiFiClient.h>
+WiFiClient wifiClient;
 const char *ssid = "CPH1723";  // SSID
 const char *password = "12345678"; // Password
 
@@ -31,38 +32,43 @@ void setup() {
 }
 
 void loop() {
- //Reading sensore value serialy from Arduino
   //================================================
-  float rom = mySerialn.readStringUntil('\r');
-  float obj = mySerialn.readStringUntil('\r');
-  float ecg = mySerialn.readStringUntil('\r');
-  Serial.println(rom);
-  Serial.println(obj);
-  Serial.println(ecg);
-if (rom == 0  && obj ==0  && ecg ==0)
+  String obj = mySerialn.readStringUntil('\r');
+  String rom = mySerialn.readStringUntil('\r');
+  String ecg = mySerialn.readStringUntil('\r');
+  Serial.print("Room tem");
+  Serial.print(rom);
+  Serial.println(" ");
+  Serial.print("Body tem");
+  Serial.print(obj);
+  Serial.println(" ");
+  Serial.print("Ecg value");
+  Serial.print(ecg);
+  Serial.print(" ");
+  if (rom == 0  && obj == 0  && ecg == 0)
   {
     Serial.println("Failed to read from sensor!");
     return;
   }
-//===============================================
-  //////////////////////////////////
+  //===============================================
  // CODE TO RETRIEVE DATA FROM SENSORS
-    
-  float body_temp = obj;
-  float room_temp = rom;
-  float ecg_data = ecg;
+
+   String body_temp = obj;
+   String room_temp = rom;
+   String ecg_data = ecg;
   //////////////////////////////////
-  
+
   //Post Data
   HTTPClient http;
   String postData;
-  postData = "body_temp=" + String(body_temp) + "&room_temp=" + String(room_temp) + "&ecg_data=" + String(ecg_data);
-    
-  http.begin("http://192.168.43.113:5000/data"); //Change `54.179.164.125` to your Private IP, keep the `:5000`         
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Remain the same 
+   postData = "body_temp=" + String(body_temp) + "&room_temp=" + String(room_temp) + "&ecg_data=" + String(ecg_data);
+ 
+  http.begin(wifiClient, "http://192.168.43.113:5000/sensors"); //Change `54.179.164.125` to your Private IP, keep the `:5000`
+  
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Remain the same
   int httpCode = http.POST(postData);   //Send the request
   String payload = http.getString();    //Get the response payload
   Serial.println(httpCode);   //httpCode == 200 means request made successfully
   http.end();  //Close connection
-    delay(5000);  //Post Data at every 5 seconds
+  delay(5000);  //Post Data at every 5 seconds
 }
